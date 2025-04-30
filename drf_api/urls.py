ef1304ed29efd5aa493ab01ec0056c01964466e8
sharanraj16 sivakumar <sharanrobo@googlemail.com>
2025-04-30 10:51:00 +0000

    likes module updated

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,4 +1,3 @@
-
 """drf_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -23,4 +22,5 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
+    path('', include('likes.urls')),
 ]
